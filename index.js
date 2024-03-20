e2f8fdd024be15305df9f42210e9d781ef5edd73
Chaia Billan <chaia_billan@yahoo.com>
2024-03-20 15:57:20 -0700

    set up express server and create get comments route

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5050;
 
+// app.use(express.json);
+
 const commentsRoutes = require('./routes/comments-routes');
 
-// all users routes
-app.use('/comments', commentsRoutes);
+// all comments routes
+app.use('/api/comments', commentsRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
