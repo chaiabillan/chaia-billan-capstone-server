be454c261c5ed93fbe7295a67625eb04d13517ca
Chaia Billan <chaia_billan@yahoo.com>
2024-04-02 16:59:50 -0700

    adjust for deployment

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -3,15 +3,15 @@ const knex = require("knex")(require("../knexfile"));
 const fetchComments = async ( req, res ) => {
     try {
 
-        const commentsWithReplies = await knex('comments')
+        const commentsWithReplies = await knex('Comments')
           // select all the rows from the comments table and then replies table 
-          .select('comments.comment_id', 'comments.username', 'comments.comment_text', 'comments.likes_count', 'comments.timestamp',
+          .select('Comments.comment_id', 'Comments.username', 'Comments.comment_text', 'Comments.likes_count', 'Comments.timestamp',
                   'replies.reply_id', 'replies.username AS reply_username', 'replies.reply_text', 'replies.likes_count AS reply_likes_count', 'replies.timestamp AS reply_timestamp')
           // joins together the replies table and the comments table 
           // retrieves everything from the left table (comments, because this is where everything is accumulated) regardless of whether anything correspondds in the right (replies) table
           // comments.comment_id indicates that the join should be based on the comment_id column of the comments table 
           // replies.comment_id indicates that the join should be based on the comment_id column of the replies table
-          .leftJoin('replies', 'comments.comment_id', 'replies.comment_id');
+          .leftJoin('replies', 'Comments.comment_id', 'replies.comment_id');
   
         // reduce method iterates over the commentsWithReplies array and groups comments with their associated replies.
         const comments = commentsWithReplies.reduce((acc, row) => {
